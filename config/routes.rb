eed71e04da631d63789fe2c8df3f6f84ed81bb52
Nick Brennan <nick_p_b@yahoo.com>
2015-09-18 17:30:12 -0700

    Step 12. created 'post' route for creatures

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get '/creatures' => 'creatures#index'
   get '/' => 'creatures#index'
   get '/creatures/new' => 'creatures#new'
+  post '/creatures' => 'creatures#create'
 end
