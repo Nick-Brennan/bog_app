08cc5366d4fb52b3df2f2edeab92109abf3e6a9c
Nick Brennan <nick_p_b@yahoo.com>
2015-09-18 18:30:05 -0700

    Step 15. added a route for creatures#show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get '/creatures' => 'creatures#index'
   get '/' => 'creatures#index'
   get '/creatures/new' => 'creatures#new'
+  get '/creatures/:id' => 'creatures#show'
   post '/creatures' => 'creatures#create'
 end
